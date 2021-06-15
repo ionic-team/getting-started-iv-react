8ee6503d9f3e4a4b4abed4139039e7c49e65746b
Eric Horodyski <horodyski@ionic.io>
2021-06-15 08:49:09 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'getting-started-iv-react',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
